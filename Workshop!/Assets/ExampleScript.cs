06be64dfdac679dc817080649d74a1a00f9fa316
Spencer Johnson <johnss18@rpi.edu>
2016-05-22 21:46:17 -0400

    added super a classy sphere to the example scene. Also added another print to the example script.

--- Workshop!/Assets/ExampleScript.cs
+++ Workshop!/Assets/ExampleScript.cs
@@ -6,6 +6,7 @@ public class ExampleScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("Why Hello There World");
+        print("Goodbye World");
 	}
 	
 	// Update is called once per frame
