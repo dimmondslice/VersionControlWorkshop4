77f4bccd99bfd0950b8c0ed9d5e96b3e2b1bbb39
Spencer <johnss18@rpi.edu>
2016-05-22 21:42:09 -0400

    Added a plane to the example scene. Also I fixed example script to represent my feelings about the world.

--- Workshop!/Assets/ExampleScript.cs
+++ Workshop!/Assets/ExampleScript.cs
@@ -6,6 +6,7 @@ public class ExampleScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("Why Hello There World");
+		print("JK world I hate you, don't ever talk to me or my son again.");
 	}
 	
 	// Update is called once per frame
